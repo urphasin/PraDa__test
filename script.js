e3bcaea537c5f382465b88bef4cd5fa04480a89b
Otito Amuga <urphasin@gmail.com>
2026-04-05 01:26:41 -0600

    dsd

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-alert("Inner Width: " + window.innerWidth + "px;");
-alert("Inner Height: " + window.innerHeight + "px;");
+// alert("Inner Width: " + window.innerWidth + "px;");
+// alert("Inner Height: " + window.innerHeight + "px;");
+
+// 1. Normal usage (most common)
+const sum = [1, 2, 3].reduce((a, b) => a + b, 0);
+
+// 2. Using it manually from the prototype
+const sum2 = Array.prototype.reduce.call([1, 2, 3], (a, b) => a + b, 0);
