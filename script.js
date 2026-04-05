5558c9d134cdbd479ec98ed19e04a68dee856bd4
Otito Amuga <urphasin@gmail.com>
2026-04-05 01:53:46 -0600

    dfdf

--- script.js
+++ script.js
@@ -6,3 +6,6 @@ const sum = [1, 2, 3].reduce((a, b) => a + b, 0);
 
 // 2. Using it manually from the prototype
 const sum2 = Array.prototype.reduce.call([1, 2, 3], (a, b) => a + b, 0);
+
+const fontFamily = getComputedStyle(document.body).fontFamily;
+alert(fontFamily);
